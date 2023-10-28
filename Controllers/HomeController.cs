d069881b83cd10adc02f2f58cb14d2f5128b3550
dsmv04 <dalton.munoz.v@hotmail.com>
2023-10-28 20:19:39 -0300

    Asignacion 5 Dia 4 Semana 7

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+using RandomPasscode.Models;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        int codeCount = HttpContext.Session.GetInt32("CodeCount") ?? 0;
+
+        // Genera un código de acceso aleatorio de 14 caracteres
+        string randomCode = GenerateRandomCode(14);
+
+        // Almacena el código en la sesión
+        HttpContext.Session.SetString("RandomCode", randomCode);
+
+        // Incrementa el contador de códigos
+        HttpContext.Session.SetInt32("CodeCount", codeCount + 1);
+
+        return View(new PasscodeViewModel
+        {
+            RandomCode = randomCode,
+            CodeCount = codeCount + 1
+        });
+    }
+
+    private string GenerateRandomCode(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random random = new Random();
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+}
---
+++ Models/PasscodeViewModel.cs
@@ -0,0 +1,9 @@
+#pragma warning disable CS8618
+namespace RandomPasscode.Models
+{
+    public class PasscodeViewModel
+    {
+        public string RandomCode { get; set; }
+        public int CodeCount { get; set; }
+    }
+}
